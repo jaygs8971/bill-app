e8a9f7dd27ea5619d24d5e756470d6ca617769cd
JAYARAM G S <jayaram.gs@mca.christuniversity.in>
2021-07-01 16:37:25 +0530

    Update orders.php
    Design changes

--- orders.php
+++ orders.php
@@ -8,43 +8,52 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
           integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
-
-    <?php
-    include('conn.php');
-    {
-    global $conn;
-    $sql = mysqli_query($conn, "SELECT * FROM orders_table");
-
-
-    while ($sql2 = mysqli_fetch_assoc($sql)) {
+</head>
+<?php
+include('conn.php');
+{
+global $conn;
+$sql = mysqli_query($conn, "SELECT * FROM orders_table");
 
 
-    $sql3 = mysqli_query($conn, "SELECT * FROM client WHERE c_id = '$sql2[c_id]'")
-    ?>
+while ($sql2 = mysqli_fetch_assoc($sql)) {
 
-    <div class="container col-lg-6 p-4 ">
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <form method="post" action="bill.php">
-                            <h6 class="card-title" style="font-size: 14px;">
-                                <?php $sql4 = mysqli_fetch_assoc($sql3);
-                                echo $sql4['name']; ?>
-                            </h6><h5 class="card-title" style="font-size: 14px">
-                                <?php echo "Invoice No: " . $sql2['o_id'];
-                                ?></h5>
-                            <h5 class="card-title" style="font-size: 14px">
-                                <?php echo "Date: " . $sql2['o_date'] ?></h5>
-                            <input type="text" hidden name="my_ord" value="<?php echo $sql2['o_id']; ?> ">
 
-                            <input type="submit" value="Check" class="btn btn-success" name="check">
-                        </form>
-                    </div>
-                </div>
+$sql3 = mysqli_query($conn, "SELECT * FROM client WHERE c_id = '$sql2[c_id]'")
+?>
+
+<div class="container col-lg-6 border mt-4 pt-4 pb-4 mb-4">
+
+
+    <!--                <div class="card">-->
+    <!--                    <div class="card-body">-->
+    <form method="post" action="bill.php">
+        <div class="row  bo">
+            <div class="col-sm-4">
+                <h6 class="card-title" style="font-size: 14px;">
+                    <?php $sql4 = mysqli_fetch_assoc($sql3);
+                    echo $sql4['name']; ?>
+                </h6>
             </div>
+            <div class="col-sm-4">
+                <h5 class="card-title" style="font-size: 14px">
+                    <?php echo "Invoice No: " . $sql2['o_id'];
+                    ?></h5>
+            </div>
+            <div class="col-sm-4">
+                <h5 class="card-title" style="font-size: 14px">
+                    <?php echo "Date: " . $sql2['o_date'] ?></h5>
+            </div>
+            <input type="text" hidden name="my_ord" value="<?php echo $sql2['o_id']; ?> ">
         </div>
-    </div>
+        <input type="submit" value="Check" class="btn btn-success mt-3" name="check">
+
+    </form>
+</div>
+</html>
+<!--    </div>-->
+<!--        </div>-->
+<!--    </div>-->
 
 <?php
 }
